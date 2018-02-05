86687b7c1638f920af0dc41ae09a21ef702acf8b
hkolli <hkolli@ftdi.com>
2018-02-05 20:45:29 +0530

    cosmetic UI changes

--- src/components/header.js
+++ src/components/header.js
@@ -4,15 +4,24 @@ import { Link } from 'react-router-dom'
 
 // The Header creates links that can be used to navigate
 // between routes.
+const style={
+  marginTop:'10px'
+}
 const Header = () => (
-  <header>
-    <nav>
-      <ul>
-      <li><Link to='/'>Home</Link></li>
-        <li><Link to='/products'>Products</Link></li>
-        <li><Link to='/orderlookup'>OrderLookUp</Link></li>
-      </ul>
-    </nav>
+  <header style={style}>
+  <nav className="navbar navbar-default">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <a className="navbar-brand" href="#">Micro Frontend POC</a>
+        </div>
+        <ul className="nav navbar-nav">
+          <li className="active"><Link to='/'>Home</Link></li>
+          <li><Link to='/products'>Products</Link></li>
+          <li><Link to='/orderlookup'>OrderLookUp</Link></li>
+        </ul>
+      </div>
+  </nav>
+ 
   </header>
 
 
